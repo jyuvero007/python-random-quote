fd2dba04b0343e97a3f339911ece2bd23e573ee5
jyuvero007 <70600786+jyuvero007@users.noreply.github.com>
2020-09-08 11:45:28 -0400

    Updated code so that quotes can now be added with the addcode() function.

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,14 @@
 import random
 
+
+def add_quote():
+    file_w = open('quotes.txt', 'a')
+    file_w.write('\nThis is a new quote')
+
 def primary():
 
 
 # print("Keep it logically awesome.")
-
 
     f = open("quotes.txt")
     quotes = f.readlines()
@@ -14,6 +18,10 @@ def primary():
     rnd = random.randint(0,last)
 
     print(quotes[rnd])
+    print(quotes[rnd + 1])
 
 if __name__ == "__main__":
     primary()
+
+    #Uncomment this function call it and add a quote. Update this function accordingly with a new quote.
+    #add_quote()
